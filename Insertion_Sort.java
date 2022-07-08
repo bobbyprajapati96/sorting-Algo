60f094df7b0e1eff6a0fa65b01f22a41fefd266f
TUSHAR PRAJAPATI <72430130+bobbyprajapati96@users.noreply.github.com>
2022-07-08 23:38:27 +0530

    Update Insertion_Sort.java

--- Insertion_Sort.java
+++ Insertion_Sort.java
@@ -4,11 +4,11 @@ public class Insertion_Sort {
     public static void main(String[] args) {
         int [] arr={12,32,4,5,72,53};
         for(int i=1;i< arr.length;i++) {
-            int current = arr[i];
-            int j=i-1;
-            while(j>=0 && current < arr[j])
+            int current = arr[i]; //32
+            int j=i-1;   // i=1 and j=1-1=0
+            while(j>=0 && current < arr[j]) //if current element( in unsorted part is less then go in
             {
-                arr[j+1]=arr[j];
+                arr[j+1]=arr[j];  
                 j--;
             }
             arr[j+1]=current;
