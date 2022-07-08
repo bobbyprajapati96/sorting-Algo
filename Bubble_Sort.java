78595a3df8519fc31b808793e89cbec13a947c07
TUSHAR PRAJAPATI <72430130+bobbyprajapati96@users.noreply.github.com>
2022-07-08 23:22:49 +0530

    Add files via upload

--- Bubble_Sort.java
+++ Bubble_Sort.java
@@ -1,23 +1,36 @@
 package Sortings;
 
 public class Bubble_Sort {
-    public static void main(String[] args) {
-        int[] arr={3,2,4,15,5,6};
-        int n=arr.length;
-        System.out.println("Before Sorting");
+    static void swap(int arr[], int i,int j)
+    {
+         int temp=arr[i];
+         arr[i]=arr[j];
+         arr[j]=temp;
+    }
+    static void printArr(int arr[])
+    {
         for(int a:arr)
         {
             System.out.print(a+" ");
         }
+    }
+    public static void main(String[] args) {
+        int[] arr={3,2,4,15,5,6};
+        int n=arr.length;
+        System.out.println("Before Sorting");
+        printArr(arr);
         //Bubble Sort
         //Time Complexity = O(n^2)
         // Not so good for competitive programming
-        for(int i=0;i<n-1;i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+        for(int i=0;i<n-1;i++)
+        {
+            boolean isswap=false;
+            for(int j=0;j<n-i-1;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    isswap=true;
+                    swap(arr,j,j+1);
                 }
             }
         }
@@ -25,17 +38,11 @@ public class Bubble_Sort {
 //        for(int i=0;i<n-1;i++) {
 //            for (int j = 0; j < n - i - 1; j++) {
 //                if (arr[j] < arr[j + 1]) {
-//                    int temp = arr[j];
-//                    arr[j] = arr[j + 1];
-//                    arr[j + 1] = temp;
+//                    swap(j,j+1);
 //                }
 //            }
 //        }
         System.out.println("\nAfter Sorting");
-
-        for(int a:arr)
-        {
-            System.out.print(a+" ");
-        }
+        printArr(arr);
     }
 }
